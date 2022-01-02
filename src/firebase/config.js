17aabb9f37bf8eb44b016cd0abbc46d64cbbd2af
iAmAli <ali.naderi72@gmail.com>
2022-01-02 11:21:30 +0330

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6BSGPQXHPVrqEpSYHZSDOc9x-zRoHO6M",
+  authDomain: "team-6adf2.firebaseapp.com",
+  projectId: "team-6adf2",
+  storageBucket: "team-6adf2.appspot.com",
+  messagingSenderId: "102327140491",
+  appId: "1:102327140491:web:ae2befc19899efa25cd48e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
